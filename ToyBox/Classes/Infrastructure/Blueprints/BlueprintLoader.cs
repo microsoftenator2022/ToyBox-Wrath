7b169f533bc8074db746270d0ebe30338efa9527
Aaron Maslen <aaron.maslen@gmail.com>
2025-09-02 17:02:30 +1000

    Support OMM mods adding blueprints to the cache on demand (https://github.com/microsoftenator2022/WrathPatches/blob/master/WrathPatches/Patches/Experimental/OnDemandOMMBlueprints.cs)

--- ToyBox/Classes/Infrastructure/Blueprints/BlueprintLoader.cs
+++ ToyBox/Classes/Infrastructure/Blueprints/BlueprintLoader.cs
@@ -146,6 +146,9 @@ namespace ToyBox {
             } else {
                 allEntries = toc.Where(item => toLoad.Contains(item.Key)).OrderBy(e => e.Value.Offset).Select(e => e.Key);
             }
+            allEntries = allEntries
+                .Concat(OwlcatModificationsManager.Instance.AppliedModifications.SelectMany(m => m.Blueprints).Select(BlueprintGuid.Parse))
+                .Distinct();
             total = allEntries.Count();
             Mod.Log($"Loading {total} Blueprints");
             _blueprints = new(total);
@@ -196,27 +199,25 @@ namespace ToyBox {
                                 int shardIndex = Math.Abs(guid.GetHashCode()) % Main.Settings.BlueprintsLoaderNumShards;
                                 var startedLoading = _startedLoadingShards[shardIndex];
                                 if (!startedLoading.TryAdd(guid, @lock)) continue;
+                                SimpleBlueprint simpleBlueprint = null;
                                 if (ResourcesLibrary.BlueprintsCache.m_LoadedBlueprints.TryGetValue(guid, out var entry)) {
                                     if (entry.Blueprint != null) {
                                         closeCountLocal++;
                                         _blueprints[entryPairA.Item2] = entry.Blueprint;
                                         continue;
                                     }
-                                } else {
-                                    continue;
-                                }
-                                if (Shared.BadBlueprints.Contains(guid.ToString()) || entry.Offset == 0U) continue;
-                                OnBeforeBPLoad(guid);
-                                stream.Seek(entry.Offset, SeekOrigin.Begin);
-                                SimpleBlueprint simpleBlueprint = null;
-                                seralizer.Blueprint(ref simpleBlueprint);
-                                if (simpleBlueprint == null) {
-                                    closeCountLocal++;
-                                    continue;
+                                    if (Shared.BadBlueprints.Contains(guid.ToString()) || entry.Offset == 0U) continue;
+                                    OnBeforeBPLoad(guid);
+                                    stream.Seek(entry.Offset, SeekOrigin.Begin);
+                                    seralizer.Blueprint(ref simpleBlueprint);
                                 }
                                 object obj;
                                 OwlcatModificationsManager.Instance.OnResourceLoaded(simpleBlueprint, guid.ToString(), out obj);
                                 simpleBlueprint = (obj as SimpleBlueprint) ?? simpleBlueprint;
+                                if (simpleBlueprint == null) {
+                                    closeCountLocal++;
+                                    continue;
+                                }
                                 entry.Blueprint = simpleBlueprint;
                                 simpleBlueprint.OnEnable();
                                 _blueprints[entryPairA.Item2] = simpleBlueprint;
